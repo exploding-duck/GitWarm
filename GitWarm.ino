8d817f0f2488d36c7db6b3be457a26fc298a369e
Matteo Coscia <matteo.a.coscia@gmail.com>
2026-04-11 14:02:47 -0600

    Refactor GitWarm.ino with updated parameters

--- GitWarm.ino
+++ GitWarm.ino
@@ -1,31 +1,30 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define DEBUG true 
+#define DEBUG false
 
-// pins
 #define ONE_WIRE_BUS 10
 #define BUTTON_PIN 9
 #define HEATER_PIN 8
 #define BATTERY_PIN A7  
-#define LED_LOWBAT 2
-#define LED_0 3
-#define LED_1 4
-#define LED_2 5
+#define LED_LOWBAT 1
+#define LED_0 2
+#define LED_1 3
+#define LED_2 6
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-float tempLow[4]  = {0, 66.0, 78.0, 91.0};
-float tempHigh[4] = {0, 68.0, 80.0, 93.0};
-int dutyCycles[4] = {0, 20, 50, 80}; 
+float tempLow[4]  = {0, 60.0, 75.0, 90.0};
+
+int dutyCycles[4] = {0, 50, 70, 100}; 
 
 int mode = 0; 
 bool lastButtonState = HIGH;
 float currentTempF = 0;
 float batteryVoltage = 0;
 
-const int pwmFrequency = 5;  
+const int pwmFrequency = 1;  
 unsigned long lastToggleTime = 0;
 bool pwmState = false;
 
@@ -53,7 +52,6 @@ void setup() {
 void loop() {
   unsigned long now = millis();
 
-  // button logic
   bool reading = digitalRead(BUTTON_PIN);
   if (lastButtonState == HIGH && reading == LOW) {
     mode = (mode + 1) % 4;
@@ -61,29 +59,21 @@ void loop() {
   }
   lastButtonState = reading;
 
-  // battery monitoring
-  if (now - lastBatteryCheck >= 15000) {
-    // temporarily shut down heater
-    bool heaterWasOn = digitalRead(HEATER_PIN);
-    digitalWrite(HEATER_PIN, LOW);
-    delay(1000);  // allow battery voltage to stabilize
-    now = millis();
+  if (now - lastBatteryCheck >= 2000) {
     int raw = analogRead(BATTERY_PIN);
-    batteryVoltage = raw * (3.3 / 1024.0) * 4.0;
+    batteryVoltage = raw * (3.3 / 1024.0) * 4.3;
     digitalWrite(LED_LOWBAT, (batteryVoltage < 10.8) ? HIGH : LOW);
-    // restore heater state
-    digitalWrite(HEATER_PIN, LOW);
-    lastToggleTime = millis();
-    lastBatteryCheck = millis();
+    lastBatteryCheck = now;
   }
 
-  // temp update
   if (now - lastTempRequest >= delayInMillis) {
     currentTempF = sensors.getTempFByIndex(0);
     sensors.requestTemperatures(); 
     lastTempRequest = now;
-    if (currentTempF < -100 || currentTempF > 200) {
-      allOff();
+
+    if (currentTempF < -58.0) {
+      digitalWrite(HEATER_PIN, LOW);
+      pwmState = false;
       return;
     }
   }
@@ -91,37 +81,38 @@ void loop() {
   if (mode == 0) {
     allOff();
   } else {
-    // leds
     digitalWrite(LED_0, (mode >= 1));
     digitalWrite(LED_1, (mode >= 2));
     digitalWrite(LED_2, (mode >= 3));
 
-    // heater logic
     unsigned long period = 1000 / pwmFrequency;
     unsigned long onTime = (period * dutyCycles[mode]) / 100;
     unsigned long offTime = period - onTime;
-    float midTemp = (tempLow[mode] + tempHigh[mode]) / 2.0;
 
-    if (currentTempF < tempLow[mode]) {
-      pwmState = true;
-      digitalWrite(HEATER_PIN, HIGH);
-    } else if (currentTempF >= midTemp) {
+    if (currentTempF >= tempLow[mode]) {
       pwmState = false;
       digitalWrite(HEATER_PIN, LOW);
     } else {
-      if (pwmState && (now - lastToggleTime >= onTime)) {
-        pwmState = false;
-        digitalWrite(HEATER_PIN, LOW);
-        lastToggleTime = now;
-      } else if (!pwmState && (now - lastToggleTime >= offTime)) {
-        pwmState = true;
+      if (dutyCycles[mode] >= 100) {
         digitalWrite(HEATER_PIN, HIGH);
-        lastToggleTime = now;
+        pwmState = true;
+      } else if (dutyCycles[mode] <= 0) {
+        digitalWrite(HEATER_PIN, LOW);
+        pwmState = false;
+      } else {
+        if (pwmState && (now - lastToggleTime >= onTime)) {
+          pwmState = false;
+          digitalWrite(HEATER_PIN, LOW);
+          lastToggleTime = now;
+        } else if (!pwmState && (now - lastToggleTime >= offTime)) {
+          pwmState = true;
+          digitalWrite(HEATER_PIN, HIGH);
+          lastToggleTime = now;
+        }
       }
     }
   }
 
-  // debugging
   if (DEBUG && (now - lastSerialPrint >= 500)) {
     Serial.print("M: "); Serial.print(mode);
     Serial.print(" | T: "); Serial.print(currentTempF);
